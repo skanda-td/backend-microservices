a8074f4cf76ebb076ee428502034c57b9b9bac7d
Skanda <skanda.td@gmail.com>
2025-12-29 19:43:07 +0530

    Logger Modification

--- notification-service/src/main/java/com/example/notificationservice/listener/OrderCreatedListener.java
+++ notification-service/src/main/java/com/example/notificationservice/listener/OrderCreatedListener.java
@@ -1,13 +1,17 @@
 package com.example.notificationservice.listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderCreatedListener {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderCreatedListener.class);
+
     @KafkaListener(topics = "order.created", groupId = "notification-group")
     public void consume(String message) {
-        System.out.println("Order created: " + message);
+        log.info("Received order created message: {}", message);
     }
 }
